50118ed7528dfde28048da8baa8d5d95465578c2
Carlos Pinzón <caph1993@gmail.com>
2021-10-07 15:34:00 +0200

    fixed errors because the Xlib library changed

--- pyrandr-bt.py
+++ pyrandr-bt.py
@@ -21,10 +21,10 @@ from Xlib import display as dpy
 from Xlib.ext import randr
 import json, time, sys
 
-
 # Randr functions
 
-def get_outputs(d): # based on refs 1 and 2
+
+def get_outputs(d: dpy.Display):  # based on refs 1 and 2
     global info
     screen = d.get_default_screen()
     info = d.screen(screen)
@@ -37,22 +37,60 @@ def get_outputs(d): # based on refs 1 and 2
             outputs[params.name] = get_output(d, params.crtc)
     return outputs
 
-MAX = 65535.0 # grabbed from ref 1
 
-def get_output(d, crtc):
+MAX = 65535.0  # grabbed from ref 1
+
+
+def get_output(d: dpy.Display, crtc):
     cg = d.xrandr_get_crtc_gamma(crtc)
+    # Ugly fix: after october 2021, cg.blue and cg.green are empty :(
+    # probably a bug in the Xlib library for Python
+    print(cg.red[-10:])
+    print(cg.blue[-10:])
+    print(cg.green[-10:])
+    alt = 1.0
+    alt = raw_red = cg.red[-1] / MAX if cg.red else alt
+    alt = raw_blue = cg.blue[-1] / MAX if cg.blue else alt
+    alt = raw_green = cg.green[-1] / MAX if cg.green else alt
     gamma = (
-        round(cg.red[-1]/MAX, 3),
-        round(cg.green[-1]/MAX, 3),
-        round(cg.blue[-1]/MAX, 3),
+        round(raw_red, 3),
+        round(raw_green, 3),
+        round(raw_blue, 3),
     )
     return dict(crtc=crtc, gamma=gamma)
 
-def set_gamma(d, output, gamma):
+
+def ugly_randr_patch(d):
+    # Ugly fix: after october 2021, the red, blue and green arguments where removed
+    # it is a bug actually, because if they are not provided, the initializer of
+    # SetCrtcGamma will complain
+
+    from Xlib.ext.randr import SetCrtcGamma, extname
+
+    def patched_set_crtc_gamma(self, crtc, size, **kwargs):
+        return SetCrtcGamma(display=self.display,
+                            opcode=self.display.get_extension_major(extname),
+                            crtc=crtc, size=size, **kwargs)
+
+    del d.display_extension_methods['xrandr_set_crtc_gamma']
+    d.extension_add_method('display', 'xrandr_set_crtc_gamma',
+                           patched_set_crtc_gamma)
+    return
+
+
+def set_gamma(d: dpy.Display, output, gamma):
     crtc = output['crtc']
     n = d.xrandr_get_crtc_gamma_size(crtc).size
-    data = [[int(MAX*i*v/(n-1)) for i in range(n)] for v in gamma]
-    d.xrandr_set_crtc_gamma(crtc, n, red=data[0], green=data[1], blue=data[2])
+    data = [[int(MAX * i * v / (n - 1)) for i in range(n)] for v in gamma]
+    rgb = {'red': data[0], 'green': data[1], 'blue': data[2]}
+    try:
+        d.xrandr_set_crtc_gamma(crtc, n, **rgb)
+        use_patch = False
+    except:
+        use_patch = True
+    if use_patch:
+        ugly_randr_patch(d)
+        d.xrandr_set_crtc_gamma(crtc, n, **rgb)
     output.update(**get_output(d, crtc))
     return
 
@@ -60,47 +98,51 @@ def set_gamma(d, output, gamma):
 # RGB vs brightness-temperature functions
 
 RGB = [
-    [1.000,  0.323,  0.000],
-    [1.000,  0.423,  0.086],
-    [1.000,  0.543,  0.166],
-    [1.000,  0.643,  0.288],
-    [1.000,  0.719,  0.428],
-    [1.000,  0.779,  0.546],
-    [1.000,  0.828,  0.648],
-    [1.000,  0.868,  0.736],
-    [1.000,  0.901,  0.814],
-    [1.000,  0.938,  0.881],
-    [1.000,  0.971,  0.943],
-    [1.000,  1.000,  1.000],
+    [1.000, 0.323, 0.000],
+    [1.000, 0.423, 0.086],
+    [1.000, 0.543, 0.166],
+    [1.000, 0.643, 0.288],
+    [1.000, 0.719, 0.428],
+    [1.000, 0.779, 0.546],
+    [1.000, 0.828, 0.648],
+    [1.000, 0.868, 0.736],
+    [1.000, 0.901, 0.814],
+    [1.000, 0.938, 0.881],
+    [1.000, 0.971, 0.943],
+    [1.000, 1.000, 1.000],
 ]
+
 
 def bt_to_rgb(brightness, temperature):
     n = len(RGB)
-    i = min(int(temperature*(n-1)), n-2)
-    d = temperature*(n-1)-i
-    rgb = [RGB[i][c]*(1-d) + RGB[i+1][c]*d for c in range(3)]
-    rgb = [round(v*brightness,3) for v in rgb]
+    i = min(int(temperature * (n - 1)), n - 2)
+    d = temperature * (n - 1) - i
+    rgb = [RGB[i][c] * (1 - d) + RGB[i + 1][c] * d for c in range(3)]
+    rgb = [round(v * brightness, 3) for v in rgb]
     return rgb
+
 
 def rgb_to_bt(red, green, blue):
     rgb = [red, green, blue]
     b = max(max(rgb), 1e-5)
-    rgb = [v/b for v in rgb]
+    rgb = [v / b for v in rgb]
+
     def dist2(t):
         rgbt = bt_to_rgb(1, t)
-        return sum((b-a)**2 for a,b in zip(rgb, rgbt))
+        return sum((b - a)**2 for a, b in zip(rgb, rgbt))
+
     tlo, thi, n = 0, 1, 10
     for repeat in range(3):
-        trange = [tlo+(thi-tlo)*i/n for i in range(n+1)]
+        trange = [tlo + (thi - tlo) * i / n for i in range(n + 1)]
         tlo, thi = sorted(sorted(trange, key=dist2)[:2])
     b = round(b, 3)
-    t = round((tlo+thi)/2, 3)
+    t = round((tlo + thi) / 2, 3)
     return b, t
 
 
 # User functions
 VERSION = 'v1.0.0'
-USAGE = f'PyRandr-BT {VERSION}\n'+"""
+USAGE = f'PyRandr-BT {VERSION}\n' + """
 Usage:
     d - demo:
         run a small demo of different combinations
@@ -146,32 +188,33 @@ Examples tl;dr:
 """.strip()
 
 
-def get_bts(d, name):
+def get_bts(d: dpy.Display, name):
     outs = get_outputs(d)
     if name is None:
         outs = outs.values()
     else:
-        outs = [v for k,v in outs.items() if k==name]
+        outs = [v for k, v in outs.items() if k == name]
         assert outs, f'Screen "{name}" not found'
     return [(out, rgb_to_bt(*out['gamma'])) for out in outs]
+
 
 def short_if(value, if_none):
     return if_none if value is None else value
 
-def user_modify_bt(
-        brightness=None, temperature=None,
-        inc_brightness=None, inc_temperature=None,
-        name=None):
+
+def user_modify_bt(brightness=None, temperature=None, inc_brightness=None,
+                   inc_temperature=None, name=None):
     d = dpy.Display()
     for out, (b, t) in get_bts(d, name):
-        b = short_if(brightness, b*100)/100
-        t = short_if(temperature, t*100)/100
-        b += short_if(inc_brightness, 0)/100
-        t += short_if(inc_temperature, 0)/100
+        b = short_if(brightness, b * 100) / 100
+        t = short_if(temperature, t * 100) / 100
+        b += short_if(inc_brightness, 0) / 100
+        t += short_if(inc_temperature, 0) / 100
         b = max(0, min(1, b))
         t = max(0, min(1, t))
         set_gamma(d, out, bt_to_rgb(b, t))
     return
+
 
 def user_list():
     d = dpy.Display()
@@ -182,19 +225,22 @@ def user_list():
         out['temperature'] = t
     return print(json.dumps(outs, indent='  '))
 
+
 def user_demo():
     print('Running demo...')
-    for db, dt in [(20,20),(-10,0), (0,-12), (10,0), (0,12)]:
+    for db, dt in [(20, 20), (-10, 0), (0, -12), (10, 0), (0, 12)]:
         for i in range(5):
             start = time.time()
             user_modify_bt(inc_brightness=db, inc_temperature=dt)
-            time.sleep(max(0, 0.05-(time.time()-start)))
+            time.sleep(max(0, 0.05 - (time.time() - start)))
     return
 
+
 def parse_num(s):
-    sign = int(s.startswith('+'))-int(s.startswith('-'))
+    sign = int(s.startswith('+')) - int(s.startswith('-'))
     value = float(s[abs(sign):])
-    return (value, None) if sign==0 else (None, sign*value)
+    return (value, None) if sign == 0 else (None, sign * value)
+
 
 def run(args):
     a = d = l = b = ib = t = it = name = None
@@ -202,21 +248,21 @@ def run(args):
         assert args, 'No arguments provided'
         args_iter = iter(args)
         for a in args_iter:
-            if a=='--list' or a=='-l':
+            if a == '--list' or a == '-l':
                 l = True
-            elif a=='--demo' or a=='-d':
+            elif a == '--demo' or a == '-d':
                 d = True
-            elif a=='--brightness' or a=='-b':
+            elif a == '--brightness' or a == '-b':
                 b, ib = parse_num(next(args_iter))
-            elif a=='--temperature' or a=='-t':
+            elif a == '--temperature' or a == '-t':
                 t, it = parse_num(next(args_iter))
-            elif a=='--combined' or a =='-c':
+            elif a == '--combined' or a == '-c':
                 b, ib = t, it = parse_num(next(args_iter))
-            elif a=='--screen' or a =='-s':
+            elif a == '--screen' or a == '-s':
                 name = next(args_iter)
-            elif a=='--help' or a=='-h':
+            elif a == '--help' or a == '-h':
                 return print(USAGE)
-            elif a=='--version' or a=='-v':
+            elif a == '--version' or a == '-v':
                 return print(VERSION)
             else:
                 raise Exception(f'argument {a} not understood')
@@ -227,15 +273,15 @@ def run(args):
             print(f'Expected argument after {a}')
         else:
             print(e)
-        return 
+        return
     if d:
         user_demo()
-    if any(x!=None for x in [b,ib,t,it]):
-        user_modify_bt(b,t,ib,it,name)
+    if any(x != None for x in [b, ib, t, it]):
+        user_modify_bt(b, t, ib, it, name)
     if l:
         user_list()
     return
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     run(sys.argv[1:])
